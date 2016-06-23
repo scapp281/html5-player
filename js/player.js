56d2a61cb4d584c0e88cef73b244cb58ad48c7c4
Stephen Chin <scapp281@gmail.com>
2016-06-23 13:55:30 -0400

    progress bar function

--- js/player.js
+++ js/player.js
@@ -3,6 +3,8 @@ window.addEventListener('load', function() {
 	video = document.getElementById('video');
 
 	playButton = document.getElementById('play-button');
+    
+    pbar = document.getElementById('pbar');
 
 	video.load();
 	video.addEventListener('canplay', function() {
@@ -16,10 +18,21 @@ window.addEventListener('load', function() {
 function playOrPause() {
 	if (video.paused) {
 		video.play();
+        update = setInterval(updatePlayer,30);
 		playButton.src = 'images/pause.png';
 	} else {
 		video.pause();
+        window.clearInterval(update);
 		playButton.src = 'images/play.png';
+        
 	}
 }
 
+function updatePlayer(){
+    var percentage = (video.currentTime/video.duration)*100;
+    pbar.style.width = percentage + '%';
+    if(video.ended){
+        window.clearInterval(update);
+        playButton.src = 'images/replay.png'
+    }
+}
