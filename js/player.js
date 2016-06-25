4ba89f70cb71862c84bcc86b773adb7321df02b4
Stephen Chin <scapp281@gmail.com>
2016-06-24 23:29:07 -0400

    muted function

--- js/player.js
+++ js/player.js
@@ -1,68 +1,84 @@
-window.addEventListener('load', function() {
+window.addEventListener('load', function () {
 
-	// Video Container
-	video = document.getElementById('video');
+    // Video Container
+    video = document.getElementById('video');
 
-	// Progress Bar Container
-	pbarContainer = document.getElementById('pbar-container');
-	pbar = document.getElementById('pbar');
+    // Progress Bar Container
+    pbarContainer = document.getElementById('pbar-container');
+    pbar = document.getElementById('pbar');
 
-	// Buttons Container
-	playButton = document.getElementById('play-button');
-	timeField = document.getElementById('time-field');
+    // Buttons Container
+    playButton = document.getElementById('play-button');
+    timeField = document.getElementById('time-field');
+    soundButton = document.getElementById('sound-button');
 
-	video.load();
-	video.addEventListener('canplay', function() {
+    video.load();
+    video.addEventListener('canplay', function () {
 
-		playButton.addEventListener('click', playOrPause, false);
-		pbarContainer.addEventListener('click', skip, false);
-		updatePlayer();
+        playButton.addEventListener('click', playOrPause, false);
+        pbarContainer.addEventListener('click', skip, false);
+        updatePlayer();
 
-	}, false);
+        soundButton.addEventListener('click', muteOrUnmute, false);
+        sbarContainer = document.getElementById('sbar-container');
+        sbar = document.getElementById('sbar');
+    }, false);
 
 }, false);
 
 function playOrPause() {
-	if (video.paused) {
-		video.play();
-		playButton.src = 'images/pause.png';
-		update = setInterval(updatePlayer, 30);
-	} else {
-		video.pause();
-		playButton.src = 'images/play.png';
-		window.clearInterval(update);
-	}
+    if (video.paused) {
+        video.play();
+        playButton.src = 'images/pause.png';
+        update = setInterval(updatePlayer, 30);
+    } else {
+        video.pause();
+        playButton.src = 'images/play.png';
+        window.clearInterval(update);
+    }
 }
 
 function updatePlayer() {
-	var percentage = (video.currentTime/video.duration)*100;
-	pbar.style.width = percentage + '%';
-	timeField.innerHTML = getFormattedTime();
-	if (video.ended) {
-		window.clearInterval(update);
-		playButton.src = 'images/replay.png';
-	}
+    var percentage = (video.currentTime / video.duration) * 100;
+    pbar.style.width = percentage + '%';
+    timeField.innerHTML = getFormattedTime();
+    if (video.ended) {
+        window.clearInterval(update);
+        playButton.src = 'images/replay.png';
+    }
 }
 
 function skip(ev) {
-	var mouseX = ev.pageX - pbarContainer.offsetLeft;
-	var width = window.getComputedStyle(pbarContainer).getPropertyValue('width');
-	width = parseFloat(width.substr(0, width.length - 2));
+    var mouseX = ev.pageX - pbarContainer.offsetLeft;
+    var width = window.getComputedStyle(pbarContainer).getPropertyValue('width');
+    width = parseFloat(width.substr(0, width.length - 2));
 
-	video.currentTime = (mouseX/width)*video.duration;
-	updatePlayer();
+    video.currentTime = (mouseX / width) * video.duration;
+    updatePlayer();
 }
 
 function getFormattedTime() {
-	var seconds = Math.round(video.currentTime);
-	var minutes = Math.floor(seconds/60);
-	if (minutes > 0) seconds -= minutes*60;
-	if (seconds.toString().length === 1) seconds = '0' + seconds;
+    var seconds = Math.round(video.currentTime);
+    var minutes = Math.floor(seconds / 60);
+    if (minutes > 0) seconds -= minutes * 60;
+    if (seconds.toString().length === 1) seconds = '0' + seconds;
 
-	var totalSeconds = Math.round(video.duration);
-	var totalMinutes = Math.floor(totalSeconds/60);
-	if (totalMinutes > 0) totalSeconds -= totalMinutes*60;
-	if (totalSeconds.toString().length === 1) totalSeconds = '0' + totalSeconds;
+    var totalSeconds = Math.round(video.duration);
+    var totalMinutes = Math.floor(totalSeconds / 60);
+    if (totalMinutes > 0) totalSeconds -= totalMinutes * 60;
+    if (totalSeconds.toString().length === 1) totalSeconds = '0' + totalSeconds;
 
-	return minutes + ':' + seconds + ' / ' + totalMinutes + ':' + totalSeconds;
+    return minutes + ':' + seconds + ' / ' + totalMinutes + ':' + totalSeconds;
+}
+
+function muteOrUnmute() {
+    if (!video.muted) {
+        video.muted = true;
+        soundButton.src = 'images/mute.png';
+        sbar.style.display = 'none';
+    } else {
+        video.muted = false;
+        soundButton.src = 'images/sound.png';
+        sbar.style.display = 'block';
+    }
 }
